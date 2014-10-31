c4b31fc9a6be6493ae0b17ea5b9efc04503b1e0a
Dean Mclaren <calmnearend@gmail.com>
2014-10-31 10:27:43 -0700

    Have working ideas

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,35 @@
+# app/controllers/ideas_controller.rb
+class IdeasController < ApplicationController
+  def index
+    @idea = Idea.all
+  end
+
+  def new
+  end
+
+  def create
+    @idea = Idea.new(params.require(:idea).permit([:title, :body]))
+    if @idea.save
+      redirect_to @idea
+    else
+      render :new
+    end
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update_attributes(params.require(:idea).permit([:title, :body]))
+      redirect_to @idea
+    else
+      render :edit
+    end
+  end
+end
